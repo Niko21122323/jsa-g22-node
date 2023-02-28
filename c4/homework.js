a6d6e049109f353bec4830a9b9a866fb4ef36222
Nikola Stojanovski <nikola211223232529321@gmail.com>
2023-02-28 17:19:39 +0100

    homework2 c4

--- c4/homework.js
+++ c4/homework.js
@@ -1,9 +1,8 @@
 import fs, { readFile } from "fs";
 //const fs = require("fs");
-const filePath = "data.json";
 const encoding = "utf8";
 
-const studenti = [
+let studenti = [
   { ime: "Bojan", prosek: 7.5, grad: "Skopje" },
   { ime: "Pero", prosek: 8.3, grad: "Bitola" },
   { ime: "Janko", prosek: 6.9, grad: "Kumanovo" },
@@ -14,10 +13,11 @@ const studenti = [
   { ime: "Stanko", prosek: 7.2, grad: "Strumica" },
 ];
 
-(async () => {
-  const dataFile = "data.json";
-  const novStudent = { name: "Ana", prosek: 9.5, grad: "Ohrid" };
+const dataFile = "data.json";
+const novStudent = { ime: "Ana", prosek: 9.5, grad: "Ohrid" };
 
+/* // Add
+(async () => {
   await new Promise((add) => {
     studenti.push(novStudent);
     add();
@@ -39,6 +39,71 @@ const studenti = [
     .catch((err) => {
       console.log("zapisot e neuspeshen", err);
     });
+})();
+
+// Modify
+(async () => {
+  await new Promise((modify) => {
+    studenti = studenti.map((student) => {
+      if (student === novStudent) {
+        return { ...novStudent, ime: "Anastasija" };
+      }
+      return student;
+    });
+    modify();
+  });
+
+  const fileWrite = (dataFile, studenti) => {
+    return new Promise((success, fail) => {
+      fs.writeFile(dataFile, JSON.stringify(studenti), (err) => {
+        if (err) return fail(err);
+        return success;
+      });
+    });
+  };
+
+  fileWrite(dataFile, studenti)
+    .then(() => {
+      console.log("zapisot e uspeshen! (vo nov file)");
+    })
+    .catch((err) => {
+      console.log("zapisot e neuspeshen", err);
+    });
+})(); */
+
+//////////////////////////////////////////////
+
+(async () => {
+  // Read existing data from file
+  await new Promise((success, fail) => {
+    fs.readFile(dataFile, (err, studenti) => {
+      if (err) return fail(err);
+      try {
+        studenti = JSON.parse(studenti);
+        success();
+      } catch (err) {
+        fail(err);
+      }
+    });
+  });
+  // Add novStudent
+  studenti.push(novStudent);
+  // Modify novStudent
+  studenti = studenti.map((student) => {
+    if (student === novStudent) {
+      return { ...novStudent, ime: "Anastasija" };
+    }
+    return student;
+  });
+  // Write updated data back to file
+  await new Promise((success, fail) => {
+    fs.writeFile(dataFile, JSON.stringify(studenti), (err) => {
+      if (err) return fail(err);
+      success();
+    });
+  });
+
+  console.log("Data updated successfully!");
 })();
 
 /* (async () => {
